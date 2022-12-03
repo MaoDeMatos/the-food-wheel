e0ee94c8beed86f47157b1f0316a422bef7b50c6
Mao De Matos <mao.dematos@gmail.com>
2022-12-03 18:37:58 +0100

    💄 Minor css update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Wheel } from './Wheel';
 
 export default function Home() {
   return (
-    <div className="px-8">
+    <div className="px-4 sm:px-8">
       <main className="flex flex-col items-center gap-8 py-8 sm:gap-12 sm:py-12">
         <h1 className="bg-gradient-to-br from-rose-300 to-purple-500 bg-clip-text text-center text-4xl font-extrabold text-transparent sm:text-6xl">
           The Food Wheel !
