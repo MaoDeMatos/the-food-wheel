9761cd1173cab951004534d85e8e2ea0e240fecf
Mao De Matos <mao.dematos@gmail.com>
2022-12-04 17:09:02 +0100

    ♻️ Fix `Wheel` sections transitions

--- src/app/Wheel.tsx
+++ src/app/Wheel.tsx
@@ -1,54 +1,34 @@
 'use client';
-import { SVG_COLORS } from '../constants';
-// import Image from 'next/image';
 
+import { useEffect, useState } from 'react';
+import { SVG_COLORS } from '../constants';
 import { useDataStoreAsync } from '../DataStore';
 
 export function Wheel() {
-  const { options } = useDataStoreAsync();
-
   return (
     <div className="aspect-square w-full overflow-hidden rounded-full shadow-xl sm:w-72 md:w-96">
-      <SvgWheel optionsCount={options.filter(Boolean).length} />
+      <SvgWheel />
       {/* <div className="h-full w-full bg-base-100/50 backdrop-blur-sm" /> */}
       {/* <Image src="/placeholder.jpg" alt="" className="object-contain" fill /> */}
     </div>
   );
 }
 
-type SvgWheelProps = { optionsCount: number };
+function SvgWheel() {
+  const { options } = useDataStoreAsync();
+  const optionsCount = options.filter(Boolean).length;
 
-function SvgWheel({ optionsCount }: SvgWheelProps) {
-  const percentage = (1 / optionsCount) * 100;
   const deg = (1 / optionsCount) * 360;
-  const dashLength = ((percentage * Math.PI * 10) / 100).toFixed(2);
+  const dashLength = ((1 / optionsCount) * Math.PI * 10).toFixed(2);
 
   // console.log({ deg });
-
-  const sections = [...new Array(optionsCount)].map((opt, idx) => {
-    return (
-      <circle
-        key={idx}
-        r="5"
-        cx="10"
-        cy="10"
-        style={{
-          fill: 'transparent',
-          strokeWidth: 10,
-          transform: `rotate(${(-90 + deg * idx).toFixed(2)}deg)`,
-          strokeDasharray: `${dashLength} ${(Math.PI * 10).toFixed(2)}`,
-        }}
-        className={`${SVG_COLORS[idx]} origin-center transition-all`}
-      />
-    );
-  });
 
   return (
     <svg
       height="20"
       width="20"
       viewBox="0 0 20 20"
-      className="h-full w-full rounded-full"
+      className="h-full w-full -rotate-90 rounded-full"
     >
       <circle
         r="10"
@@ -56,7 +36,42 @@ function SvgWheel({ optionsCount }: SvgWheelProps) {
         cy="10"
         className="fill-neutral transition-colors"
       />
-      {sections}
+      {[...new Array(optionsCount)]
+        .map((opt, idx) => {
+          return (
+            <Section key={idx} index={idx} dashLength={dashLength} deg={deg} />
+          );
+        })
+        .reverse()}
     </svg>
+  );
+}
+
+type SectionProps = {
+  index: number;
+  deg: number;
+  dashLength: string;
+};
+
+function Section({ index, deg, dashLength }: SectionProps) {
+  const [isInitialRender, setInitialRender] = useState(true);
+
+  useEffect(() => {
+    setInitialRender(false);
+  }, []);
+
+  return (
+    <circle
+      r="5"
+      cx="10"
+      cy="10"
+      style={{
+        transform: isInitialRender
+          ? `rotate(360deg)`
+          : `rotate(${(deg * index).toFixed(2)}deg)`,
+        strokeDasharray: `${dashLength} ${(Math.PI * 10).toFixed(2)}`,
+      }}
+      className={`${SVG_COLORS[index]} origin-center fill-transparent stroke-[10] transition-all duration-300`}
+    />
   );
 }
