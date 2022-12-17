a35ca43f8507c12ca868fe44fd5c978a22bb3213
Mao De Matos <mao.dematos@gmail.com>
2022-12-17 19:52:45 +0100

    ✨ Add GitHub link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 import { LeftMenu } from './LeftMenu';
 import { RightMenu } from './RightMenu';
 import { WheelContainer } from './WheelContainer';
+import { GitHub } from 'react-feather';
 
 export default function Home() {
   return (
     <div>
       <main className="flex flex-col items-center gap-12 px-4 py-8 sm:gap-24 sm:px-8 sm:py-12">
+        <a
+          href="https://github.com/MaoDeMatos/the-food-wheel"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn-sm btn-circle btn absolute top-3 right-3 flex items-center justify-center p-1"
+          title="Check out the code here !"
+        >
+          <GitHub />
+        </a>
+
         <div>
           <h1 className="bg-gradient-to-r from-primary to-accent bg-clip-text text-center text-4xl font-extrabold !leading-tight text-transparent sm:text-6xl">
             The Food Wheel !
