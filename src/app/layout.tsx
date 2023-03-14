10f9d573e94fd040dabda6cf29566c5c35461dde
Mao De Matos <mao.dematos@gmail.com>
2023-03-14 23:16:41 +0100

    ♻️ Refactor `head.tsx` according to `next` update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,20 @@
+import { Metadata } from 'next';
+
 // import '../build/styles.css'; // Used when Turbopack is enabled
 import './globals.css'; // Used when Turbopack is disabled
+
+export const metadata: Metadata = {
+  title: 'The Food Wheel',
+  description:
+    "Don't know where to eat ? The food wheel will tell you where to go !",
+  icons: {
+    icon: '/favicon.ico',
+  },
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: '#282a36' },
+  ],
+};
 
 export default function RootLayout({
   children,
@@ -8,10 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+      {/* <head /> will contain components hydrated by the metadata constant. Find out more at https://beta.nextjs.org/docs/api-reference/metadata */}
       <head />
       <body>{children}</body>
     </html>
