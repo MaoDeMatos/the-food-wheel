182f6829830c06811e70e8732d5668a4db08b320
Mao De Matos <mao.dematos@gmail.com>
2023-05-20 19:19:25 +0200

    ✨ Add `ThemeSelector` on `Home` page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,30 @@
 import { LeftMenu } from './LeftMenu';
 import { RightMenu } from './RightMenu';
+import { ThemeSelector } from './ThemeSelector';
 import { WheelContainer } from './WheelContainer';
 import { GitHub } from 'react-feather';
+
+function VerticalDivider() {
+  return <div className="h-8 w-px bg-neutral-content" />;
+}
 
 export default function Home() {
   return (
     <div>
-      <main className="flex flex-col items-center gap-12 px-4 py-8 sm:gap-24 sm:px-8 sm:py-12">
-        <a
-          href="https://github.com/MaoDeMatos/the-food-wheel"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="btn-sm btn-circle btn absolute top-3 right-3 flex items-center justify-center p-1"
-          title="Check out the code here !"
-        >
-          <GitHub />
-        </a>
+      <main className="flex flex-col items-center gap-12 px-4 py-12 sm:gap-24 sm:px-8 sm:py-14">
+        <div className="fixed top-3 right-3 z-10 flex gap-4">
+          <ThemeSelector />
+          <VerticalDivider />
+          <a
+            href="https://github.com/MaoDeMatos/the-food-wheel"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn-sm btn-circle btn flex items-center justify-center p-1"
+            title="Check out the code here !"
+          >
+            <GitHub />
+          </a>
+        </div>
 
         <div>
           <h1 className="bg-gradient-to-r from-primary to-accent bg-clip-text text-center text-4xl font-extrabold !leading-tight text-transparent sm:text-6xl">
