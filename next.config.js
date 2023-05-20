c8be5dcf626593846a8e6712621113d1a96e9459
Mao De Matos <mao.dematos@gmail.com>
2023-05-20 19:39:25 +0200

    🔧 Change `next.config` according to last upgrade

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    typedRoutes: true,
-  },
+  reactStrictMode: true,
+  poweredByHeader: false,
 };
 
 module.exports = nextConfig;
