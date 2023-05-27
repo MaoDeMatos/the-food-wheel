fdec7a6140ace74d09ffdcf04435515467b656e1
Mao De Matos <mao.dematos@gmail.com>
2023-05-27 23:51:00 +0200

    ♻️ Refactor dropzone component

--- src/app/_wheel/ChangeWheelImageButton.tsx
+++ src/app/_wheel/ChangeWheelImageButton.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useMemo, useState } from 'react';
-import { FileMinus, Image } from 'react-feather';
-import { DropZone, DropZoneProps } from '../../components/DropZone';
-import { useDataStoreAsync, setImage } from '../../utils/DataStore';
+import { FileMinus, FilePlus } from 'react-feather';
+import { useDataStoreAsync, setImage } from '@/utils/DataStore';
+import { WheelImageModal } from './WheelImageModal';
 
 export function ChangeWheelImage() {
   const { image } = useDataStoreAsync();
-  const [isModalOpen, setModalIsOpen] = useState(false);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [error, setError] = useState<Error | null>(null);
 
   const { hasImage, Icon } = useMemo(() => {
@@ -13,28 +14,9 @@ export function ChangeWheelImage() {
 
     return {
       hasImage,
-      Icon: hasImage ? FileMinus : Image,
+      Icon: hasImage ? FileMinus : FilePlus,
     };
   }, [image]);
-
-  const dropHandler: DropZoneProps['onDrop'] = (e) => {
-    const dt = e.dataTransfer;
-    const files = dt.files;
-    const firstFile = files?.[0];
-
-    if (firstFile?.type.startsWith('image/')) {
-      setImage(URL.createObjectURL(firstFile));
-      setModalIsOpen(false);
-    } else {
-      setError(
-        new Error(
-          `File extension not supported. ${
-            Boolean(firstFile?.type) ? `("${firstFile?.type}")` : ''
-          } `
-        )
-      );
-    }
-  };
 
   useEffect(() => {
     if (hasImage || !isModalOpen) setError(null);
@@ -45,34 +27,19 @@ export function ChangeWheelImage() {
       <button
         type="button"
         className="btn-primary btn-circle btn absolute -top-0 right-0 z-[1] flex p-1"
-        onClick={() => (hasImage ? setImage(null) : setModalIsOpen(true))}
+        title={hasImage ? 'Remove image' : 'Add image'}
+        onClick={() => (hasImage ? setImage(null) : setIsModalOpen(true))}
       >
         <Icon />
         <span className="sr-only">Change wheel image</span>
       </button>
 
-      <div className={`modal ${isModalOpen ? 'modal-open' : null}`}>
-        <div className="modal-box relative flex flex-col gap-6 bg-neutral text-neutral-content">
-          <button
-            type="button"
-            className="btn-sm btn-circle btn absolute right-2 top-2"
-            onClick={() => setModalIsOpen(false)}
-          >
-            ✕
-          </button>
-          <h3 className="text-lg font-bold">Change wheel image here</h3>
-          <DropZone
-            content={
-              <>
-                Drop an image here to put it on the wheel
-                <br />
-                {error && error.message}
-              </>
-            }
-            onDrop={dropHandler}
-          />
-        </div>
-      </div>
+      <WheelImageModal
+        isOpen={isModalOpen}
+        setIsOpen={setIsModalOpen}
+        error={error}
+        setError={setError}
+      />
     </>
   );
 }
