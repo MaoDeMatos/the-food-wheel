4f3472081ccdde2129335fa88e4dcf1e01b44981
Mao De Matos <mao.dematos@gmail.com>
2023-05-27 19:54:21 +0200

    ♻️ Move top-right fixed toolbar to the `layout`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { Work_Sans } from 'next/font/google';
 
 // import '../build/styles.css'; // Used when Turbopack is enabled
 import './globals.css'; // Used when Turbopack is disabled
+import { GitHub } from 'react-feather';
+import { ThemeSelector } from './ThemeSelector';
 
 const work_sans = Work_Sans({
   subsets: ['latin'],
@@ -53,10 +55,29 @@ export default function RootLayout({
       </head>
 
       <body className="antialiased h-full p-1.5">
+        <div className="fixed right-4 top-4 z-10 flex gap-4">
+          <ThemeSelector />
+          <VerticalDivider />
+
+          <a
+            href="https://github.com/MaoDeMatos/the-food-wheel"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn-sm btn-circle btn flex items-center justify-center p-1 ring-0 ring-transparent ring-offset-2 ring-offset-base-100"
+            title="Check out the code here !"
+          >
+            <GitHub />
+          </a>
+        </div>
+
         <div className="h-full overflow-auto rounded-3xl bg-base-100">
           {children}
         </div>
       </body>
     </html>
   );
+}
+
+function VerticalDivider() {
+  return <div className="h-8 w-px bg-neutral-content" />;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,10 @@
 import { LeftMenu } from './LeftMenu';
 import { RightMenu } from './RightMenu';
-import { ThemeSelector } from './ThemeSelector';
 import { WheelContainer } from './_wheel/WheelContainer';
-import { GitHub } from 'react-feather';
-
-function VerticalDivider() {
-  return <div className="h-8 w-px bg-neutral-content" />;
-}
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center gap-12 p-4 pt-12 sm:gap-24 sm:p-8 sm:pt-14">
-      <div className="fixed right-4 top-4 z-10 flex gap-4">
-        <ThemeSelector />
-        <VerticalDivider />
-
-        <a
-          href="https://github.com/MaoDeMatos/the-food-wheel"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="btn-sm btn-circle btn flex items-center justify-center p-1"
-          title="Check out the code here !"
-        >
-          <GitHub />
-        </a>
-      </div>
-
       <div>
         <h1 className="bg-gradient-to-r from-primary to-accent bg-clip-text text-center text-4xl font-extrabold !leading-tight text-transparent sm:text-6xl">
           The Food Wheel !
